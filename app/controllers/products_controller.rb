716ecdf44f0a745cd3701afe58621d9a2fd17c81
Westin Walker <westinwalker1988@gmail.com>
2023-07-10 12:19:59 -0600

    installs cors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,7 +21,7 @@ class ProductsController < ApplicationController
 
   def update
     @product = Product.find_by(id: params["id"])
-    @product = Product.update(
+    @product.update(
       name: params["name"] || @product.name,
       price: params["price"] || @product.price,
       image_url: params["image_url"] || @product.image_url,
@@ -31,8 +31,9 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    product = Product.find_by(id: params["id]"])
-    product.destroy
+    @product = Product.find_by(id: params["id"])
+    @product.destroy
+
     render json: { message: "Entry has been deleted." }
   end
 end
