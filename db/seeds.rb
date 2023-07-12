43f50689c3f900eef3449d91eb004ac90575adcc
Westin Walker <westinwalker1988@gmail.com>
2023-07-12 13:06:27 -0600

    adds stock attribute to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 product = Product.new(name: "Bicycle Platinum Playing Cards", price: 12, image_url: "https://playingcarddecks.com/cdn/shop/products/1322d168e52aa6e68245118705e4710a_original_e4caca7b-466f-47f4-b687-1ef895d8bb11.jpg?v=1576804703", description: "From a lineage of design eloquence comes a new deck of playing cards rendered in platinum. The elements of the design are forged from liquid metal tones, and enhanced by metallic inks. Crafted with rare aesthetics, the Bicycle Platinum is a work of sophistication engineered to last and impress.")
 product.save
 
-product = Product.new(name: "NooTrees Bamboo 3-ply Bathroom Tissue", price: 18 image_url: "https://m.media-amazon.com/images/I/81btHbuhvFL._AC_SL1500_.jpg", description: "Environmentally friendly right from the core to the outer packaging, NooTrees 3-ply Bathroom Rolls are made with 100 percent bamboo pulp. Its bamboo fibres are velvety soft and ultra absorbent (at least 20 percent more than wood pulp).")
+product = Product.new(name: ""Wildspire Fantasy Miniatures"", price: 45 image_url: "https://m.media-amazon.com/images/I/91gNZwc-1-L.__AC_SX300_SY300_QL70_FMwebp_.jpg", description: "A collection of 68 paintable miniatures for D&D that live up to the creativity of your games. Versatile figurines for D&D, used as monsters, player characters or NPCs. Great gifts for any D&D enthusiast!")
 product.save
 
 product = Product.new(name: "QMay DND Dice Set", price: 23, image_url: "https://m.media-amazon.com/images/I/81AgXWno1YL.__AC_SX300_SY300_QL70_FMwebp_.jpg", description: "Super deal: 20  D&D dice sets, indluce 5 double-color sets (140 pieces in total), and 1 big black drawstring bag!")
