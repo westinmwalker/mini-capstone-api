7a6187f66d93fb8243e4056d6f4419568b980711
Westin Walker <westinwalker1988@gmail.com>
2023-07-12 12:43:09 -0600

    changes price to decimal and description to text; adds stock attribute

---
+++ db/migrate/20230712183718_make_changes_to_products.rb
@@ -0,0 +1,6 @@
+class MakeChangesToProducts < ActiveRecord::Migration[7.0]
+  def change
+    change_column :products, :description, :text
+    change_column :products, :price, :decimal, precision: 7, scale: 2
+  end
+end
